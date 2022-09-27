1c30a5b72604a6b1f5b0c9d5c8ab0a22151db055
Aayush Sinha <aayush.aryan@me.com>
2022-09-27 19:01:46 +0200

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="card footer">
+      <h2>GitFind | Developed by Aayush &amp; Team | &copy; 2022</h2>
+      <a href="https://github.com/aayusharyan/gitfind" target="_blank" rel="noreferrer">Github Source Code</a>
+      <p> Made with ❤️ &amp; 💻</p>
+    </div>
+  )
+}
+
+export default Footer;
